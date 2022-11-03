ef8f6a410fb26d209a75bbe4f5d872eb5ad42f13
Abbas-Yasir <yasir.m.o.abbas@gmail.com>
2022-11-03 14:21:26 +0900

    Initial commit

---
+++ Core/Inc/w25qxxConf.h
@@ -0,0 +1,10 @@
+#ifndef _W25QXXCONFIG_H
+#define _W25QXXCONFIG_H
+											  //Shard Local
+#define _W25QXX_SPI                   hspi4   //hspi1 hspi4
+#define _W25QXX_CS_GPIO               GPIOE   //gpiaA gpiaE
+#define _W25QXX_CS_PIN                GPIO_PIN_4
+#define _W25QXX_USE_FREERTOS          0
+#define _W25QXX_DEBUG                 1
+
+#endif
---
+++ Debug/Core/tjpg/tjpgd.d
@@ -0,0 +1,2 @@
+Core/tjpg/tjpgd.o: ../Core/tjpg/tjpgd.c ../Core/tjpg/tjpgd.h
+../Core/tjpg/tjpgd.h:
